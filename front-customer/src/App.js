e9481b21fab2637adc1cbc77b3434c99b37a4a43
hoon <csh3004@naver.com>
2024-11-25 11:01:45 +0900

    feat: customer, employee

---
+++ front-customer/src/App.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Sidebar from './components/Sidebar';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Request from './pages/Request';
+import ProductSubscription from './pages/ProductSubscription';
+import Inquiry from './pages/InsuranceInquiry';
+
+
+function App() {
+  return (
+    <Router>
+      <div className="app">
+        <Navbar />
+        <Sidebar />
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/subscription" element={<ProductSubscription />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/inquiry" element={<Inquiry />} />
+            <Route path="/request" element={<Request />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
