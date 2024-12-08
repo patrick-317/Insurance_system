298c1c1828c3ec014ef4e3437111cfdca7a40c57
Patrick._. <151355761+patrick-317@users.noreply.github.com>
2024-12-08 20:08:51 +0900

    rdsReplica added

--- src/main/java/com/example/insurance_system/config/MyBatisConfig.java
+++ src/main/java/com/example/insurance_system/config/MyBatisConfig.java
@@ -1,10 +1,50 @@
 package com.example.insurance_system.config;
 
+import jakarta.annotation.PostConstruct;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+
+import javax.sql.DataSource;
 
 @Configuration
-@MapperScan("com.example.insurance_system")
+@MapperScan(basePackages = "com.example.insurance_system.insurance.repository")
 public class MyBatisConfig {
 
+    @Primary
+    @Bean(name = "masterDataSource")
+    @ConfigurationProperties(prefix = "spring.datasource.master")
+    public DataSource masterDataSource() {
+        return DataSourceBuilder.create().build();
+    }
+
+    @Bean(name = "replicaDataSource")
+    @ConfigurationProperties(prefix = "spring.datasource.replica")
+    public DataSource replicaDataSource() {
+        return DataSourceBuilder.create().build();
+    }
+
+    @Primary
+    @Bean(name = "masterSqlSessionFactory")
+    public SqlSessionFactory masterSqlSessionFactory(@Qualifier("masterDataSource") DataSource masterDataSource) throws Exception {
+        SqlSessionFactoryBean sessionFactory = new SqlSessionFactoryBean();
+        sessionFactory.setDataSource(masterDataSource);
+        sessionFactory.setMapperLocations(new PathMatchingResourcePatternResolver().getResources("classpath:mapper/*.xml"));
+        return sessionFactory.getObject();
+    }
+
+    @Bean(name = "replicaSqlSessionFactory")
+    public SqlSessionFactory replicaSqlSessionFactory(@Qualifier("replicaDataSource") DataSource replicaDataSource) throws Exception {
+        SqlSessionFactoryBean sessionFactory = new SqlSessionFactoryBean();
+        sessionFactory.setDataSource(replicaDataSource);
+        sessionFactory.setMapperLocations(new PathMatchingResourcePatternResolver().getResources("classpath:mapper/*.xml"));
+        return sessionFactory.getObject();
+    }
 }
--- src/main/java/com/example/insurance_system/insurance/service/ContractService.java
+++ src/main/java/com/example/insurance_system/insurance/service/ContractService.java
@@ -9,7 +9,10 @@ import com.example.insurance_system.insurance.entity.enumeration.Insurance.Insur
 import com.example.insurance_system.insurance.repository.ContractMapper;
 import com.example.insurance_system.insurance.repository.CustomerMapper;
 import com.example.insurance_system.insurance.repository.InsuranceMapper;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -25,15 +28,22 @@ public class ContractService {
     private final InsuranceMapper insuranceMapper;
     private final CustomerMapper customerMapper;
 
-    public ContractService(ContractMapper contractMapper,
-                           InsuranceMapper insuranceMapper, CustomerMapper customerMapper) {
+    private final SqlSessionFactory replicaSqlSessionFactory;
+
+    @Autowired
+    public ContractService(
+            ContractMapper contractMapper,
+            InsuranceMapper insuranceMapper,
+            CustomerMapper customerMapper,
+            @Qualifier("replicaSqlSessionFactory") SqlSessionFactory replicaSqlSessionFactory) {
         this.contractMapper = contractMapper;
         this.insuranceMapper = insuranceMapper;
         this.customerMapper = customerMapper;
+        this.replicaSqlSessionFactory = replicaSqlSessionFactory;
     }
+
     @Transactional
     public void applyForInsurance(CustomerDTO customer, int insuranceId) {
-        System.out.println("Customer Gender: " + customer.getGender());
         Insurance insurance = findInsuranceById(insuranceId);
         customerMapper.insertCustomer(customer);
 
@@ -46,9 +56,11 @@ public class ContractService {
     }
 
     public List<Insurance> getInsuranceListByType(String typeName) {
-        return insuranceMapper.findByType(typeName);
+        try (SqlSession sqlSession = replicaSqlSessionFactory.openSession()) {
+            InsuranceMapper mapper = sqlSession.getMapper(InsuranceMapper.class);
+            return mapper.findByType(typeName);
+        }
     }
-
 
     public Insurance findInsuranceById(int insuranceId) {
         Insurance insurance = insuranceMapper.findById(insuranceId);
@@ -58,56 +70,30 @@ public class ContractService {
         return insurance;
     }
 
-    private InsuranceType typeChoiceToInsuranceType(String typeChoice) {
-        switch (typeChoice) {
-            case "1": return LIFE_INSURANCE;
-            case "2": return DAMAGE_INSURANCE;
-            case "3": return THIRD_INSURANCE;
-            default:
-                throw new IllegalArgumentException("유효하지 않은 보험 유형입니다.");
-        }
-    }
-
-//    @Autowired
-//    private ContractMapper contractMapper;
-
-    @Autowired
-    @Lazy
-    private InsuranceService insuranceService;
-
-    // 계약 여부 확인
     public boolean checkCustomerInsuranceContract(Integer customerId, Integer insuranceId) {
         return contractMapper.checkCustomerInsuranceContract(customerId, insuranceId);
     }
 
-    // 계약 정보 조회 후 보험료에 따른 금액 설정
     public Contract getContractByCustomerAndInsurance(Integer customerId, Integer insuranceId) {
         Contract contract = contractMapper.getContractByCustomerAndInsurance(customerId, insuranceId);
 
         if (contract != null) {
-            // 보험료를 가져와서 계약 금액을 설정
-            String premium = insuranceService.getInsurancePremium(insuranceId);
-            contract.setPremium(premium); // 보험료를 계약 금액으로 설정
+            String premium = contract.getPremium();
+            contract.setPremium(premium);
         }
 
         return contract;
     }
 
-    // 계약 저장
     public void saveContract(Contract contract) {
         contractMapper.insertContract(contract);
     }
 
-    // 계약 업데이트
     public void updateContract(Contract contract) {
         contractMapper.updateContract(contract);
     }
 
-    // 계약 삭제
     public void deleteContract(Integer customerId, Integer insuranceId) {
         contractMapper.deleteContract(customerId, insuranceId);
     }
-
-
-
 }
